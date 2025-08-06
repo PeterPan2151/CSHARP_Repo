30dbeaa6e3c9dc7bf0e93e7968c9f3b8a5a54da4
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-08-06 16:22:36 -0600

    Add files via upload

---
+++ Addition_Calculator/Program.cs
@@ -0,0 +1,8 @@
+﻿// Prints
+Console.WriteLine("Enter a text: ");
+   
+// gets input and assigns it to a variable
+string userInput = Console.ReadLine();
+
+// String concatenation
+Console.WriteLine("You entered: " + userInput);
---
+++ Hello_world/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Declare a variable
+string myFriendsName;
+myFriendsName = "Angel";
+Console.WriteLine(myFriendsName);
