c793967ca49bc2a682bc04bb9246b9dffefe9131
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-10-14 19:38:25 -0600

    Add files via upload

--- Hello_world/Program.cs
+++ Hello_world/Program.cs
@@ -1,7 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-// Declare a variable
-string myFriendsName;
-myFriendsName = "Angel";
-Console.WriteLine(myFriendsName);
+﻿// This is a comment!
+Console.WriteLine("Hello, Rafael");
