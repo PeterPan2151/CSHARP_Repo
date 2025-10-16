00ac72580bc0aae2f7bb1de4b637561f27b829ab
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-10-16 12:00:31 -0600

    Add files via upload

---
+++ Datatypes/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Reference type
+string myName = "Peter";
+
+// Value types
+int myAge = 25;
+double pi = 3.14;
+
+sbyte age = -15;
+
+short myFriends = 2312;
+
+float pi2 = 3.1416f;
+
+Console.ReadKey();
