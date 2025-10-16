b3087b25a7187027850d5b31ce9919e9b451c7b9
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-10-15 19:54:35 -0600

    Add files via upload

---
+++ Variables/Program.cs
@@ -0,0 +1,11 @@
+﻿// This is a comment!
+Console.WriteLine("Hello, Rafael");
+
+// declare a variable
+string myFriendsName;
+// assigned a value to the variable
+myFriendsName = "Peter";
+
+Console.WriteLine(myFriendsName);
+
+Console.ReadKey();
